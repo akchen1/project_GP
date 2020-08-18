514fba2d97f8f7313f95907cd830741471f4d41b
Siddharth Dandekar <sdandeka@ualberta.ca>
2020-08-18 13:09:05 -0500

    Added a main and pause menu
    Stuff is pretty basic for now but it all works

---
+++ Project GP/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+
+    public GameObject pauseMenuUI;
+
+    public float PreviousTimeScale;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+       
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Project GP/Assets/Scripts/StartAndQuitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartAndQuitGame : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
